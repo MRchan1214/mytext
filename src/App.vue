c374839908ee1cb852801ea3a6a0b9b1f89e66d1
Your Name <MRchan>
2019-10-07 19:33:32 +0800

    222

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>这是app根组件</h1>
+    <index></index>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import index from './views/index'
+export default {
+  components: {
+    index: index
+  }
+}
+</script>
+<style lang="less">
+</style>
---
+++ src/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 导入element框架
+import ElementUI from 'element-ui'
+// 导入element样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 使用路由
+Vue.use(Router)
+// 使用element
+Vue.use(ElementUI)
+
+export default new Router({
+  routes: [
+
+  ]
+})
